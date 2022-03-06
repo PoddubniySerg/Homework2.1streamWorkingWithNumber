2f73e0c53af3cfb8cfc5efa2d7309ce5a0785ec5
Poddubniy Sergey <poddubniysergey1981@gmail.com>
2022-03-06 17:19:44 +0300

    new Homework 2.1 streamAPI

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Collections.sort(intList);
+        for (int number : intList) {
+            System.out.print(number > 0 && number % 2 == 0 ? number + " " : "");
+        }
+    }
+}
